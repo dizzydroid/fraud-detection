a9c863d438ded9e7658418157981690aac92c84b
Abdelrahman Hany <boody753107@gmail.com>
2025-04-28 08:30:08 +0300

    feat: added unit test for each metric

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,9 +1,24 @@
 import pytest
+import json
 import numpy as np
 from sklearn.metrics import recall_score
 
 @pytest.fixture
-def loaded_data() -> tuple[np.ndarray, np.ndarray]:
+def load_metrics() -> dict[str, float]:
+    """ Loads model output's metrics
+
+    Returns:
+        dict[str, float]: holds the metric key and values
+    """
+
+    file_path = "../results/metrics.json"
+    with open (file_path, "r") as f:
+        metrics = json.load(f)
+        
+    return metrics
+
+@pytest.fixture
+def load_data() -> tuple[np.ndarray, np.ndarray]:
     """Load model output for metric tests.
 
     Returns:
@@ -15,9 +30,21 @@ def loaded_data() -> tuple[np.ndarray, np.ndarray]:
     y_pred = np.load('artifacts/y_pred.npy')
     return y_true, y_pred
 
-def test_recall(loaded_data: tuple[np.ndarray, np.ndarray]) -> None:
+def test_recall(load_metrics: dict[str, float]) -> None:
     """ Test function to ensure ensemble prediction recall to be 
-        greater than or equal to 0.93 (worst case in reference paper)"""
-    y_true, y_pred = loaded_data
-    recall = recall_score(y_true, y_pred)
-    assert recall >= 0.93, f"Recall too low: {recall}"
+        greater than or equal to 1 (worst case in reference paper)"""
+    metrics = load_metrics
+    assert metrics['recall'] >= 1, f"Recall too low: {metrics['recall']}"
+
+def test_precision(load_metrics: dict[str, float]) -> None:
+    """ Test function to ensure ensemble prediction recall to be 
+        greater than or equal to 1 (worst case in reference paper)"""
+    metrics = load_metrics
+    assert metrics['precision'] >= 0.0656, f"Recall too low: {metrics['precision']}"
+
+def test_accuracy(load_metrics: dict[str, float]) -> None:
+    """ Test function to ensure ensemble prediction recall to be 
+        greater than or equal to 1 (worst case in reference paper)"""
+    metrics = load_metrics
+    assert metrics['accuracy'] >= 0.9989, f"Recall too low: {metrics['accuracy']}"
+
